51cfa35a6b2c407d0d842023fc827e6876fe18d4
Filipe Sá <0101567@softinsa.corp>
2023-03-01 17:08:50 +0000

    checkpoint

--- app/pods/components/ui/container/navigation/component.js
+++ app/pods/components/ui/container/navigation/component.js
@@ -5,13 +5,25 @@ import wait from '../../../../../utils/wait';
 
 export default class UiContainerNavigationComponent extends Component {
   @tracked className = 'opacity-100 left-0 max-height-100';
+  @tracked locked = false;
+  @tracked buttons = [1, 2, 3, 4];
 
   @action async enterNavigation() {
+    if (this.locked) return;
+    this.locked = true;
+
     this.className = 'transition-0 opacity-0 left-200 max-height-0';
     this.className = 'opacity-100 left-0 max-height-100';
+
+    await wait(this.buttons.length * 75 + 400);
+    this.locked = false;
   }
   @action async leaveNavigation() {
+    if (this.locked) return;
+    this.locked = true;
     this.className = 'transition-0 opacity-100 left-0 max-height-100';
     this.className = ' opacity-0 left-200 max-height-0';
+    await wait(this.buttons.length * 75 + 400);
+    this.locked = false;
   }
 }
