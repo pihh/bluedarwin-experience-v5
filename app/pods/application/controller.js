59480a9f94b4b11ef2f884979d5a5fc188226717
unknown <filipemotasa@hotmail.com>
2023-03-23 10:25:51 +0000

    cursor

--- app/pods/application/controller.js
+++ app/pods/application/controller.js
@@ -2,6 +2,13 @@ import Controller from '@ember/controller';
 import { service } from '@ember/service';
 import { action } from '@ember/object';
 
+import MouseFollower from "mouse-follower";
+import gsap from "gsap";
+
+MouseFollower.registerGSAP(gsap);
+
+
+
 export default class ApplicationController extends Controller {
   @service('components/ui/background') background;
   @service experience;
@@ -9,5 +16,10 @@ export default class ApplicationController extends Controller {
 
   @action didInsertCanvas($el) {
     this.experience.load($el);
+    const cursor = new MouseFollower({
+      container: '#bde-main',
+      speed: 0.2
+    });
+    
   }
 }
